7ab6ffe0602b2977a15857703498f8529d787c82
fireyyyyy <firey.yibrah@gmail.com>
2023-04-25 12:34:45 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,7 +14,7 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-       	else if (n < 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
@@ -22,8 +22,8 @@ int print_sign(int n)
 	else
 	{
 		_putchar(48);
-		return
+		return (0);
 	}
-	-putchar('\n');
+	_putchar('\n');
 }
 
